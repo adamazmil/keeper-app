9916028560ebecde8510bfa5e59ccb80335982a7
Adam Azmil <muhd.adam1@gmail.com>
2021-10-06 13:12:44 -0500

    feat(components): Added note component

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className = "note">
+            <h1> This is a note's title</h1>
+            <p1> This is a note's content</p1>
+        </div>
+    );
+}
+
+export default Note;
