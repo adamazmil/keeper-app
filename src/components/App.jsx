dfb70143b016239766820b9fdca24d4f4b142874
Adam Azmil <muhd.adam1@gmail.com>
2021-10-07 10:25:31 -0500

    feat(App): noteCreate function built. Maps json data to note component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import Heading from "./Heading"
 import Footer from "./Footer"
 import Note from "./Note"
+import notes from "../notes";
+
+function createNote(notes){
+    return(
+        <Note
+        key = {notes.key}
+        title = {notes.title}
+        content = {notes.content}
+        />
+    );
+
+
+}
 function App(){
     return (
         <div>
           <Heading />
-            <Note />
+            {notes.map(createNote)}
             <Footer />
         </div>
 
